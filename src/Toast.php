47e9f8db0994b3fee75bfd79e9fbd2adf537c1a0
Muhammed Sari <24608797+mabdullahsari@users.noreply.github.com>
2023-01-15 15:53:25 +0100

    Remove type based static factories

--- src/Toast.php
+++ src/Toast.php
@@ -17,26 +17,6 @@ final readonly class Toast implements JsonSerializable
         return ToastBuilder::create();
     }
 
-    public static function error(): ToastBuilder
-    {
-        return ToastBuilder::create()->error();
-    }
-
-    public static function info(): ToastBuilder
-    {
-        return ToastBuilder::create()->info();
-    }
-
-    public static function success(): ToastBuilder
-    {
-        return ToastBuilder::create()->success();
-    }
-
-    public static function warning(): ToastBuilder
-    {
-        return ToastBuilder::create()->warning();
-    }
-
     public function clone(string $replacement): self
     {
         return new self(new Message($replacement), $this->duration, $this->type);
