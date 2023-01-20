39de31cf0c9cadac422339925fd701b408833188
Muhammed Sari <mabdullahsari@hotmail.com>
2023-01-20 21:38:20 +0100

    Drop worker idea

--- resources/js/hub.js
+++ resources/js/hub.js
@@ -1,19 +1,12 @@
 import { Toast } from './toast';
-import { Worker } from './worker';
 
 export function Hub(Alpine) {
     Alpine.data('toasterHub', (initialToasts, config) => {
-        const worker = Worker.configure(config.duration);
-
         return {
             _toasts: [],
 
             get toasts() {
                 return this._toasts.filter(t => ! t.trashed);
-            },
-
-            flush() {
-                this._toasts = [];
             },
 
             init() {
@@ -24,21 +17,13 @@ export function Hub(Alpine) {
                 for (const toast of initialToasts) {
                     this.show(toast);
                 }
-
-                this.isEmpty = this.isEmpty.bind(this);
-                this.flush = this.flush.bind(this);
-            },
-
-            isEmpty() {
-                return ! this.toasts.length;
             },
 
             show(toast) {
                 toast = Alpine.reactive(Toast.fromJson(toast));
-                this._toasts.push(toast);
-
                 toast.runAfterDuration(toast => toast.dispose());
-                worker.start({ onLoop: this.isEmpty, onTerminate: this.flush });
+
+                this._toasts.push(toast);
             },
         }
     });
