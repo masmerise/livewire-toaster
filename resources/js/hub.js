1e2c95c863a7d34cfe3142697ff6e05c7c85f21d
Joe <104938042+lrljoe@users.noreply.github.com>
2026-03-30 12:49:38 +0100

    Separate the functionality for processing a received toaster event (#76)
    * Separate the functionality for processing a received toaster event to enable direct referenceing
    
    * Refactor toast event handling in hub.js
    
    ---------
    
    Co-authored-by: Muhammed Sarı <hi@muhammedsari.me>

--- resources/js/hub.js
+++ resources/js/hub.js
@@ -20,20 +20,24 @@ export function Hub(Alpine) {
 
             init() {
                 document.addEventListener('toaster:received', event => {
-                    const toast = Toast.fromJson({ duration: config.duration, ...event.detail });
-
-                    if (config.replace) {
-                        this.toasts.filter(t => t.equals(toast)).forEach(t => t.dispose());
-                    } else if (config.suppress && this.toasts.some(t => t.equals(toast))) {
-                        return;
-                    }
-
-                    this.show(toast);
+                    this.processToast(event);
                 });
 
                 initialToasts.map(Toast.fromJson).forEach(toast => this.show(toast));
             },
+            
+            processToast(data) {
+                const toast = Toast.fromJson({ duration: config.duration, ...data.detail });
+                
+                if (config.replace) {
+                    this.toasts.filter(t => t.equals(toast)).forEach(t => t.dispose());
+                } else if (config.suppress && this.toasts.some(t => t.equals(toast))) {
+                    return;
+                }
 
+                this.show(toast);
+            },
+            
             show(toast) {
                 toast = Alpine.reactive(toast);
                 toast.runAfterDuration(toast => toast.dispose());
